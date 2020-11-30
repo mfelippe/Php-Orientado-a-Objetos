0169f09f13a9d2aac53fcbef7bffacba47586e23
Marcos Felippe <mfelippe.bsb@hotmail.com>
2020-11-30 14:21:21 -0300

    criando metodo de acessos e construtores

--- index.php
+++ index.php
@@ -5,14 +5,29 @@
     <title>Php orientado</title>
 </head>
 <body>
+    <h1> Projeto de encapsulamento</h1>
     <pre><?php
     # pegando o modelo de classes
     require_once 'classes/classes.php';
-    #criando o objeto com a classe caneta
-    $c1 = new Caneta("bic","preta",0.7);
+    require_once 'interfaces/Controlador.php';
+    $c = new ControleRemoto();
+    $c->ligar();
+    $c->maisVolume();
+    $c->abrirMenu();
+    echo "<hr>";
 
-    print_r($c1)
 
+    //trabalhando com objeto composto: class Lutador
+    $l = array();
+    $l[0] =new Lutador("Pretty Boy", "França", 30,1.75,70,11,12,2);
+    $l[1] =new Lutador("Putcript", "Brasil", 29,1.68,60,14,2,3);
+    $l[2] =new Lutador("Felippe", "Brasil", 29,1.68,60,14,2,3);
+
+    // laço em for para mostrar todos os lutadores cadastrados
+    $fim = count($l);
+    for($i=0; $i<1 ;$i++) {
+        $l[$i]->apresentar();
+    }
     ?></pre>
 </body>
 </html>
